c0fd8bb52aa0828cbeb5895f669dd2b2db9c74c1
Mikael Brevik <mikaelbre@gmail.com>
2014-10-13 15:01:24 +0200

    Adds tests and ci integration

---
+++ tests/immstruct_test.js
@@ -0,0 +1,55 @@
+var chai = require('chai');
+chai.should();
+
+var immstruct = require('../');
+
+var EventEmitter = require('events').EventEmitter;
+
+describe('immstruct', function () {
+
+  afterEach(function () {
+    immstruct.clear();
+  });
+
+  it('should return an event emitter', function () {
+    immstruct('customKey').should.be.instanceof(EventEmitter);
+  });
+
+  it('should create empty immutable structure and random key if no input', function () {
+    var structure = immstruct();
+    structure.current.toJS().should.be.an('object');
+    structure.key.should.be.an('string');
+  });
+
+  it('should give structure with random key when js object given as only argument', function () {
+    var structure = immstruct({ foo: 'hello' });
+    structure.current.toJS().should.have.property('foo');
+    structure.key.should.be.an('string');
+  });
+
+  it('should give structure with key and default object if only key is given and structure dont exist', function () {
+    var structure = immstruct('customKey');
+    structure.current.toJS().should.be.an('object');
+    structure.key.should.equal('customKey');
+  });
+
+  it('should give structure with key and default object if only key is given and structure dont exist', function () {
+    var structure = immstruct('customKey2');
+    structure.current.toJS().should.be.an('object');
+    structure.key.should.not.equal('customKey');
+  });
+
+  it('should be able to retrieve created structure with same key', function () {
+    immstruct('customKey', { foo: 'hello' });
+    immstruct('customKey').current.toJS().should.have.property('foo');
+    immstruct('customKey').current.toJS().foo.should.equal('hello');
+  });
+
+  it('should clear all instances', function () {
+    immstruct('customKey', { 'foo': 'hello' });
+    immstruct('customKey').current.toJS().should.have.property('foo');
+    immstruct.clear();
+    immstruct('customKey').current.toJS().should.not.have.property('foo');
+  });
+
+})
---
+++ tests/structure_test.js
@@ -0,0 +1,88 @@
+var chai = require('chai');
+chai.should();
+
+var Immutable = require('immutable');
+var Structure = require('../src/structure');
+
+describe('immstruct', function () {
+
+  it('should trigger swap when structure is changed', function (done) {
+    var structure = new Structure({
+      data: { 'foo': 'hello' }
+    });
+    structure.on('swap', done);
+
+    structure.cursor(['foo']).update(function () {
+      return 'bar';
+    });
+  });
+
+  it('should trigger change with data when existing property is changed', function (done) {
+    var structure = new Structure({
+      data: { 'foo': 'hello' }
+    });
+
+    structure.on('change', function (path, newValue, oldValue) {
+      path.should.eql(['foo']);
+      oldValue.should.equal('hello');
+      newValue.should.equal('bar');
+      done();
+    });
+
+    structure.cursor(['foo']).update(function () {
+      return 'bar';
+    });
+
+    structure.current.toJS().should.eql({
+      foo: 'bar'
+    });
+  });
+
+  it('should trigger add with data when existing property is added', function (done) {
+    var structure = new Structure({
+      data: { 'foo': 'hello' }
+    });
+
+    structure.on('add', function (path, newValue) {
+      path.should.eql(['bar']);
+      newValue.should.equal('baz');
+      done();
+    });
+
+    structure.cursor(['bar']).update(function (state) {
+      return 'baz';
+    });
+    structure.current.toJS().should.eql({
+      foo: 'hello',
+      bar: 'baz'
+    });
+  });
+
+  it('should trigger delete with data when existing property is removed', function (done) {
+    var structure = new Structure({
+      data: { 'foo': 'hello' }
+    });
+
+    structure.on('delete', function (path, oldValue) {
+      path.should.eql(['foo']);
+      oldValue.should.equal('hello');
+      done();
+    });
+
+    structure.cursor().remove('foo').toJS().should.eql({});
+  });
+
+  it('should expose immutable.js cursor', function () {
+    var structure = new Structure({
+      data: { 'foo': 'hello' }
+    });
+
+    var cursor = structure.cursor(['foo']);
+    cursor.deref().should.equal('hello');
+    cursor = cursor.update(function () {
+      return 'bar';
+    });
+    cursor.deref().should.equal('bar');
+  });
+
+});
